2864689163d828cdfdfd6bae1db27e5d3de851e4
Jordan <jordanculver92@yahoo.com>
2013-11-27 12:51:01 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YearlyResolutionsApp::Application.config.secret_key_base = '0d66454a9f09f8d896580c0929df87f881445b569f4936f653fc0ac45e3c79f5fb27b4e10282057fb1228ea96da83b54bfd37d0d1890c42a1e8e3b2958c399f7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+YearlyResolutionsApp::Application.config.session_store :cookie_store, key: '_yearly_resolutions_app_session'
